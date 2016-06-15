1c11954cefe16d797d287ebf231348ba7e90bdd8
Beep <Beep@HP>
2016-06-15 14:29:12 +0200

    Version 1.0

---
+++ Maven/Iteracion1/src/main/java/org/Iteracion1/MostrarProductos.java
@@ -0,0 +1,9 @@
+package org.Iteracion1;
+import java.util.List;
+public class MostrarProductos {
+	private List<Producto> productos;
+	
+	public List<Producto> listarproductos(){
+		return productos;
+	}
+}
