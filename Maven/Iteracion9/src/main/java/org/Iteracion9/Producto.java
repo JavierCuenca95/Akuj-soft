31743a611cf2373af7cd8510aa61eef326c49255
Beep <Beep@HP>
2016-06-17 00:01:19 +0200

    Version 1.0

---
+++ Maven/Iteracion9/src/main/java/org/Iteracion9/Producto.java
@@ -0,0 +1,28 @@
+//Version 1.0
+package org.Iteracion9;
+
+public class Producto {
+	private String Nombre;
+	private double Precico;
+	public Producto(String nombre, double precico) {
+		super();
+		Nombre = nombre;
+		Precico = precico;
+	}
+	@Override
+	public String toString() {
+		return "Producto [Nombre=" + Nombre + ", Precico=" + Precico + "]";
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public double getPrecico() {
+		return Precico;
+	}
+	public void setPrecico(double precico) {
+		Precico = precico;
+	}
+}
