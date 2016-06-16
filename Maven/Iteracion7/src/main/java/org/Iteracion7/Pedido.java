e004d1ebc5b8b7df7abcd45f6a4bc43e3cba1e1c
Beep <Beep@HP>
2016-06-16 23:49:46 +0200

    Version 4.1

---
+++ Maven/Iteracion7/src/main/java/org/Iteracion7/Pedido.java
@@ -0,0 +1,18 @@
+//Version 4.1
+package org.Iteracion7;
+import java.util.List;
+import java.util.ArrayList;
+public class Pedido {
+	private Servidor server;
+	private long id;
+	private List<Producto> productos=new ArrayList();
+	public Pedido() {
+		super();
+	}
+	public void anadirproductopedido(Producto p){
+		productos.add(p);
+	}
+	public long getId(){
+		return id;
+	}
+}
